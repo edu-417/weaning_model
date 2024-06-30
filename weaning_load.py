af219e3708854ad65e334294d85db4e42a466a35
yyayaCalero <yngridjoan@gmail.com>
2024-06-30 13:48:11 -0500

    update model

--- weaning_load.py
+++ weaning_load.py
@@ -1,6 +1,61 @@
 import numpy as np
 import pandas as pd
 import tflite_runtime.interpreter as tflite
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+
+data = pd.read_excel("dataset_weaning.xlsx")
+data["Ph"]=data["Ph"].replace(",",".",regex=True).astype(float)
+
+data.loc[data.Ph>=9,"PhClass"]=1
+data.loc[data.Ph<9,"PhClass"]=0
+
+seventy_fifth_vt=data["vt"].quantile(0.75)
+twenty_fifth_vt=data["vt"].quantile(0.25)
+data_vt_iqr=seventy_fifth_vt-twenty_fifth_vt
+upper_limit=seventy_fifth_vt+1.5*data_vt_iqr
+lower_limit=twenty_fifth_vt-1.5*data_vt_iqr
+data.loc[(data.vt>upper_limit) | (data.vt<lower_limit),"vtClass"]=1
+data.loc[(data.vt<=upper_limit) & (data.vt>=lower_limit),"vtClass"]=0
+
+seventy_fifth_DP=data["DP"].quantile(0.75)
+twenty_fifth_DP=data["DP"].quantile(0.25)
+data_DP_iqr=seventy_fifth_DP-twenty_fifth_DP
+upper_limit=seventy_fifth_DP+1.5*data_DP_iqr
+lower_limit=twenty_fifth_DP-1.5*data_DP_iqr
+data.loc[(data.DP>upper_limit) | (data.DP<lower_limit),"DPClass"]=1
+data.loc[(data.DP<=upper_limit) & (data.DP>=lower_limit),"DPClass"]=0
+
+
+seventy_fifth_PEEP=data["PEEP"].quantile(0.75)
+twenty_fifth_PEEP=data["PEEP"].quantile(0.25)
+data_PEEP_iqr=seventy_fifth_PEEP-twenty_fifth_PEEP
+upper_limit=seventy_fifth_PEEP+1.5*data_PEEP_iqr
+lower_limit=twenty_fifth_PEEP-1.5*data_PEEP_iqr
+data.loc[(data.PEEP>upper_limit) | (data.PEEP<lower_limit),"PEEPClass"]=1
+data.loc[(data.PEEP<=upper_limit) & (data.PEEP>=lower_limit),"PEEPClass"]=0
+
+seventy_fifth_Ppausa=data["Ppausa"].quantile(0.75)
+twenty_fifth_Ppausa=data["Ppausa"].quantile(0.25)
+data_Ppausa_iqr=seventy_fifth_Ppausa-twenty_fifth_Ppausa
+upper_limit=seventy_fifth_Ppausa+1.5*data_Ppausa_iqr
+lower_limit=twenty_fifth_Ppausa-1.5*data_Ppausa_iqr
+data.loc[(data.Ppausa>upper_limit) | (data.Ppausa<lower_limit),"PpausaClass"]=1
+data.loc[(data.Ppausa<=upper_limit) & (data.Ppausa>=lower_limit),"PpausaClass"]=0
+
+data=data[data.dias<50]
+
+X = data.drop(columns=['dias'])
+y = data['dias']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+
+
+
 
 # Ruta al archivo del modelo .tflite
 model_path = 'weaning_model.tflite'
@@ -15,7 +70,7 @@ output_details = interpreter.get_output_details()
 
 # Preprocesar los datos de entrada (ajusta según tu modelo)
 # Suponiendo que X_test es un numpy array con los datos de prueba
-X_test = pd.read_csv("X_test.csv").values
+
 
 # Realizar inferencia en una muestra de prueba
 input_data = np.array(X_test[0], dtype=np.float32)
@@ -27,6 +82,6 @@ interpreter.invoke()
 # Obtener los resultados de la inferencia
 output_data = interpreter.get_tensor(output_details[0]['index'])
 
-print("X_test 0:", X_test[0])
-print("Input_details 0:", input_details[0])
-print("Output_details 0:", output_details[0])
+print("output_data : ",output_data)
+
+
